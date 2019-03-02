fe87d4562e410adaa5562ca0bce32d3b09c988c1
Ali Thompson <alithom123@gmail.com>
2019-03-02 10:17:03 -0600

    spotify api working

--- liri.js
+++ liri.js
@@ -12,40 +12,100 @@ var fs = require("fs");
 var Spotify = require('node-spotify-api');
 //module.exports = CastMember ???
 
-// axios.get('http://img.omdbapi.com/?i=tt3896198&r=json&apikey=90c3533d')
-axios.get('http://omdbapi.com/?i=tt3896198&r=json&apikey=90c3533d')
-    .then(function (response) {
-        console.log("SUCCESSFUL AXE");
-        console.log(response.data);
-        console.log(response.status);
-        console.log(response.headers);
+
+var action = process.argv[2];
+var value = process.argv[3];
+
+// Spotify Variables
+var trackName = value;
 
 
-        // resultElement.innerHTML = generateSuccessHTMLOutput(response);
-    })
-    .catch(function (error) {
-        console.log("UNSUCCESSFUL AXE");
-        console.log(error);
-        // resultElement.innerHTML = generateErrorHTMLOutput(error);
+// node liri.js spotify-this-song '<song name here>'
+// Artist(s)
+// The song's name
+// A preview link of the song from Spotify
+// The album that the song is from
+// If no song is provided then your program will default to "The Sign" by Ace of Base.
+
+var hitSpotify = function (_trackName) {
+
+    // Set fefault song if _trackName is empty.
+    if (!_trackName) _trackName = "The Sign";
+
+    var spotify = new Spotify({
+        //     SPOTIFY_ID=1cece788870c44dd93ee1ba75313f349
+        // SPOTIFY_SECRET=53c0e77758c44504936fc412612bd454
+        id: "1cece788870c44dd93ee1ba75313f349",
+        secret: "53c0e77758c44504936fc412612bd454"
     });
 
-var spotify = new Spotify({
-    //     SPOTIFY_ID=1cece788870c44dd93ee1ba75313f349
-    // SPOTIFY_SECRET=53c0e77758c44504936fc412612bd454
-    id: "1cece788870c44dd93ee1ba75313f349",
-    secret: "53c0e77758c44504936fc412612bd454"
-});
+    spotify.search({
+        type: 'track',
+        query: _trackName
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
 
-spotify.search({
-    type: 'track',
-    query: 'All the Small Things'
-}, function (err, data) {
-    if (err) {
-        return console.log('Error occurred: ' + err);
+        console.log(`Artist: ${data.tracks.items[0].artists[0].name}`);
+
+        // console.log(data);
+        console.log(data.tracks.items[0]);
+
+    });
+}
+
+
+if (action) {
+
+    switch (action) {
+        case 'spotify-this-song':
+            hitSpotify(trackName);
+            break;
+        case 'concert-this':
+            // hitSpotify();
+            break;
+        case 'movie-this':
+            // hitSpotify();
+            break;
+        case 'do-what-it-says':
+            // hitSpotify();
+            break;
     }
+} else {
+    printHelp();
+}
 
-    console.log(data);
-});
+var help = function printHelp() {
+    console.log(`
+                            Incorrect command format.Here are some samples:
+                            node liri.js spotify - this - song 'Take This Waltz'
+                            node liri.js concert - this 'Dave Matthews Band'
+                            node liri.js movie - this 'You Have Mail'
+                            node liri.js do -what - it - says `);
+};
+
+
+
+var hitAxios = function () {
+
+    // axios.get('http://img.omdbapi.com/?i=tt3896198&r=json&apikey=90c3533d')
+    axios.get('http://omdbapi.com/?i=tt3896198&r=json&apikey=90c3533d')
+        .then(function (response) {
+            console.log("SUCCESSFUL AXE");
+            console.log(response.data);
+            console.log(response.status);
+            console.log(response.headers);
+
+
+            // resultElement.innerHTML = generateSuccessHTMLOutput(response);
+        })
+        .catch(function (error) {
+            console.log("UNSUCCESSFUL AXE");
+            console.log(error);
+            // resultElement.innerHTML = generateErrorHTMLOutput(error);
+        });
+}
 
 
 
@@ -86,31 +146,6 @@ spotify.search({
 
 // Date of the Event (use moment to format this as "MM/DD/YYYY")
 
-// node liri.js spotify-this-song '<song name here>'
-
-// This will show the following information about the song in your terminal/bash window
-
-// Artist(s)
-
-// The song's name
-
-// A preview link of the song from Spotify
-
-// The album that the song is from
-
-// If no song is provided then your program will default to "The Sign" by Ace of Base.
-
-// You will utilize the node-spotify-api package in order to retrieve song information from the Spotify API.
-
-// The Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a client id and client secret:
-
-// Step One: Visit https://developer.spotify.com/my-applications/#!/
-
-// Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
-
-// Step Three: Once logged in, navigate to https://developer.spotify.com/my-applications/#!/applications/create to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
-
-// Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the node-spotify-api package.
 
 // node liri.js movie-this '<movie name here>'
 
